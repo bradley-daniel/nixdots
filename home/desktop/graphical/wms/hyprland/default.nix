40fb677b7c201aa6b4ee4747cb649b3851eb71cf
bradley-daniel <danielbradley.2020@gmail.com>
2023-12-30 05:22:43 -0500

    update configuration

--- home/desktop/graphical/wms/hyprland/default.nix
+++ home/desktop/graphical/wms/hyprland/default.nix
@@ -7,9 +7,32 @@
   inactive_border = "rgba(ffffffee)";
 in {
   home.sessionVariables = {
-    QT_QPA_PLATFORM = "wayland";
+    NIXOS_OZONE_WL = "1";
+    # QT_QPA_PLATFORMTHEME = "gtk3";
+    # QT_SCALE_FACTOR = "1";
+    #MOZ_ENABLE_WAYLAND = "1";
     SDL_VIDEODRIVER = "wayland";
+    # _JAVA_AWT_WM_NONREPARENTING = "1";
+    # QT_QPA_PLATFORM = "wayland-egl";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    # WLR_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card0";
+    WLR_NO_HARDWARE_CURSORS = "1"; # if no cursor,uncomment this line
+    # GBM_BACKEND = "nvidia-drm";
+    CLUTTER_BACKEND = "wayland";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    LIBVA_DRIVER_NAME = "nvidia";
+    WLR_RENDERER = "vulkan";
+    __NV_PRIME_RENDER_OFFLOAD = "1";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
     XDG_SESSION_TYPE = "wayland";
+    GTK_USE_PORTAL = "1";
+    NIXOS_XDG_OPEN_USE_PORTAL = "1";
+    XDG_CACHE_HOME = "\${HOME}/.cache";
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    XDG_BIN_HOME = "\${HOME}/.local/bin";
+    XDG_DATA_HOME = "\${HOME}/.local/share";
   };
   wayland.windowManager.hyprland = {
     enable = true;
@@ -20,24 +43,24 @@ in {
     settings = {
       "$mainMod" = "SUPER";
 
-      env = ''
-        env = XDG_SESSION_TYPE,wayland
-        env = XDG_SESSION_DESKTOP,Hyprland
-
-        env = GDK_BACKEND,wayland
-        env = QT_QPA_PLATFORM,wayland
-        env = QT_QPA_PLATFORMTHEME,qt5ct
-        env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
-        env = QT_AUTO_SCREEN_SCALE_FACTOR,1
-        env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
-
-        env = SDL_VIDEODRIVER,wayland
-        env = _JAVA_AWT_WM_NONREPARENTING,1
-        env = WLR_NO_HARDWARE_CURSORS,1
-        env = WLR_DRM_NO_ATOMIC,1
-
-        env = OZONE_PLATFORM,wayland
-      '';
+      # env = ''
+      #   env = XDG_SESSION_TYPE,wayland
+      #   env = XDG_SESSION_DESKTOP,Hyprland
+      #
+      #   env = GDK_BACKEND,wayland
+      #   env = QT_QPA_PLATFORM,wayland
+      #   env = QT_QPA_PLATFORMTHEME,qt5ct
+      #   env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
+      #   env = QT_AUTO_SCREEN_SCALE_FACTOR,1
+      #   env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
+      #
+      #   env = SDL_VIDEODRIVER,wayland
+      #   env = _JAVA_AWT_WM_NONREPARENTING,1
+      #   env = WLR_NO_HARDWARE_CURSORS,1
+      #   env = WLR_DRM_NO_ATOMIC,1
+      #
+      #   env = OZONE_PLATFORM,wayland
+      # '';
       monitor = [
         "DP-2,2560x1440@144,1920x0,1"
         "DP-1,1920x1200,0x0,1"
--- hosts/bradley/configuration.nix
+++ hosts/bradley/configuration.nix
@@ -98,15 +98,41 @@
 
   environment = {
     variables = {
-      XCURSOR_THEME = "macOS-BigSur";
-      XCURSOR_SIZE = "32";
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      WLR_NO_HARDWARE_CURSORS = "1"; # if no cursor,uncomment this line
+      NIXOS_OZONE_WL = "1";
+      # QT_QPA_PLATFORMTHEME = "gtk3";
+      # QT_SCALE_FACTOR = "1";
+      #MOZ_ENABLE_WAYLAND = "1";
+      SDL_VIDEODRIVER = "wayland";
+      # _JAVA_AWT_WM_NONREPARENTING = "1";
+      # QT_QPA_PLATFORM = "wayland-egl";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      # WLR_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card0";
+      # GBM_BACKEND = "nvidia-drm";
+      CLUTTER_BACKEND = "wayland";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      LIBVA_DRIVER_NAME = "nvidia";
+      WLR_RENDERER = "vulkan";
+      __NV_PRIME_RENDER_OFFLOAD = "1";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+      GTK_USE_PORTAL = "1";
+      NIXOS_XDG_OPEN_USE_PORTAL = "1";
+      XDG_CACHE_HOME = "\${HOME}/.cache";
+      XDG_CONFIG_HOME = "\${HOME}/.config";
+      XDG_BIN_HOME = "\${HOME}/.local/bin";
+      XDG_DATA_HOME = "\${HOME}/.local/share";
+      # XCURSOR_THEME = "macOS-BigSur";
+      # XCURSOR_SIZE = "32";
+      # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      # QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     };
 
-    sessionVariables = {
-      NIXOS_OZONE_WL = "1"; # Hint electron apps to use wayland
-    };
+    # sessionVariables = {
+    #   NIXOS_OZONE_WL = "1"; # Hint electron apps to use wayland
+    # };
   };
 
   services.xserver.videoDrivers = ["nvidia"];
