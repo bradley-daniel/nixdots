7cf1ae54c429f3e2730dcd772a6f0b61ffbbdf07
bradley-daniel <danielbradley.2020@gmail.com>
2024-02-15 22:57:39 -0500

    updated file path

--- hosts/bradley/configuration.nix
+++ hosts/bradley/configuration.nix
@@ -7,8 +7,7 @@
 }: {
   imports = [
     # Include the results of the hardware scan.
-    # ./hardware-configuration.nix
-    ./tmp/hardware-configuration.nix
+    ./hardware-configuration.nix
   ];
 
   # Bootloader.
