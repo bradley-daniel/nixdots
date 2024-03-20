f2605a2ee275945fdfd0f8d3b7d523811155a0ad
danielbradley.2020@gmail.com <danielbradley.2020@gmail.com>
2024-03-20 15:53:22 -0400

    updated hardware-configuration

--- hosts/bradley/hardware-configuration.nix
+++ hosts/bradley/hardware-configuration.nix
@@ -8,23 +8,23 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4867d15a-fec8-4143-8f3b-bfc71d42547b";
+    { device = "/dev/disk/by-uuid/8b16bb27-c0d1-4480-9322-acccca1f4bef";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/EAE2-45C7";
+    { device = "/dev/disk/by-uuid/5418-C012";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/5aa8246c-1cca-4243-99d1-10d4bdf4e7a5"; }
+    [ { device = "/dev/disk/by-uuid/1a77dd7f-8d04-42b2-b56d-46c221187587"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -32,7 +32,8 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp7s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
